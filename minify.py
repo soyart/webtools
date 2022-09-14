326baa0c384a99148b0f8e3507af0b71aae280cd
prem <pphansuriyanon@gmail.com>
2022-09-15 03:24:11 +0700

    nothing(minify.py): pretty printing

--- minify.py
+++ minify.py
@@ -4,6 +4,8 @@ import os
 import sys
 import htmlmin
 
+prog_name = os.path.basename(__file__)
+
 def loop_dir(dir_name, start_path):
     # Save previous CWD before chdir
     prev = os.getcwd()
@@ -35,13 +37,13 @@ def loop_dir(dir_name, start_path):
         if os.path.abspath("..") != start_path:
             os.chdir("..")
         else:
-            print("> DONE")
+            print("[{prog_name}] > DONE")
             exit()
 
 visited = {}
 def read_and_minify(html_filename):
-    fullpath = os.path.abspath(html_filename)
-    this_visited = visited.get(fullpath)
+    full_path = os.path.abspath(html_filename)
+    this_visited = visited.get(full_path)
     if this_visited:
         return
 
@@ -50,26 +52,28 @@ def read_and_minify(html_filename):
     fp_r.close()
 
     minified = htmlmin.minify(document)
-    print(f"> HTML: {html_filename}")
+    print(f"[{prog_name}] Minify: {full_path}")
     fp_w = open(html_filename, "w")
     fp_w.write(minified)
     fp_w.close()
 
-    visited[fullpath] = True
+    visited[full_path] = True
 
 if len(sys.argv) < 2:
-    print("Missing HTML root directory")
-    print("Usage: minify.py <HTML_DIR>")
+    print("[{prog_name}] Missing HTML root directory")
+    print("Usage: {prog_name} <HTML_DIR>")
     exit()
 
 root_dir = sys.argv[1]
-print(f'HTML directory: {root_dir}')
+print(f'[{prog_name}] HTML directory: {root_dir}')
 
 path_len = len(root_dir)
 trailing = root_dir[path_len-1]
 if trailing == "/":
   root_dir = root_dir[:path_len-1]
 
-root_fullpath = os.path.abspath(root_dir)
+root_full_path = os.path.abspath(root_dir)
 before_root = os.path.abspath("..")
-loop_dir(root_fullpath, before_root)
+
+# call loop_dir (recursive)
+loop_dir(root_full_path, before_root)
