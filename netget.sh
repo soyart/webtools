5cfa09efeb6ba4f1339eeb9f71db01df535e229e
prem <pphansuriyanon@gmail.com>
2022-09-22 02:32:04 +0700

    refactor(netget.sh): use get_shtools

--- netget.sh
+++ netget.sh
@@ -2,18 +2,6 @@
 
 # TODO: refactor
 
-downloader() {
-	URL=$1;
-	save_name="$2/$3"
-	echo "downloading from $URL to $save_name";
-
-	mkdir -p $2;
-	command -v ftp && ftp -Vo "$save_name" "$URL"\
-		|| command -v curl && curl $URL > "$save_name";
-
-	chmod u+x $save_name;
-}
-
 # Download using ftp or curl to bin/ssg
 # Since ssg is not from me, you may need to check its content first.
 get_ssg() {
@@ -36,26 +24,42 @@ get_ssg() {
 		echo "bin/ssg: ok"
 	fi
 }
-get_lb() {
-	if [ ! -x bin/lb.sh ]; then
-		echo "missing bin/lb.sh"\
-		&& echo "downloading lb.sh from gitlab.com/artnoi/unix"\
-		&& downloader "https://gitlab.com/artnoi/unix/-/raw/main/sh-tools/bin/lb.sh" "bin" "lb.sh"\
-		&& echo "lb.sh downloaded to bin"
+
+downloader() {
+	URL=$1;
+	save_name="$2/$3"
+	echo "downloading from $URL to $save_name";
+
+	mkdir -p $2;
+	command -v ftp && ftp -Vo "$save_name" "$URL"\
+		|| command -v curl && curl $URL > "$save_name";
+
+	chmod u+x $save_name;
+}
+
+get_shtools() {
+	target_dir="$1";
+	target_file="$2";
+	download_url="$3";
+
+	if [ ! -x "$target" ]; then
+		echo "missing $target"\
+		&& echo "downloading $target from $download_url"\
+		&& downloader "$download_url" "$target_dir" "$target_file"\
+		&& echo "$target_file downloaded to $target_dir from $download_url"
 	else
-		echo "bin/lb.sh: ok"
+		echo "$target_dir/$target_file: ok"
 	fi
 }
 
+get_lb() {
+	get_shtools "bin" "lb.sh"\
+		"https://gitlab.com/artnoi/unix/-/raw/main/sh-tools/bin/lb.sh";
+}
+
 get_yn() {
-	if [ ! -x bin/yn.sh ]; then
-		echo "missing bin/yn.sh"\
-		&& echo "downloading yn.sh from gitlab.com/artnoi/unix"\
-		&& downloader "https://gitlab.com/artnoi/unix/-/raw/main/sh-tools/bin/yn.sh" "bin" "yn.sh"\
-		&& echo "yn.sh downloaded to bin"
-	else
-		echo "bin/yn.sh: ok"
-	fi
+	get_shtools "bin" "yn.sh"\
+		"https://gitlab.com/artnoi/unix/-/raw/main/sh-tools/bin/yn.sh";
 }
 
 get_unix() {
