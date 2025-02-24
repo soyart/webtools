0564872762159f5b83fc09f2659f01a013ec6e68
soyart <soyart@users.noreply.github.com>
2025-02-24 20:28:23 +0000

    deploy: eb2ef6c73c6d7d23f85af76423950eccd1806bed

--- script.js
+++ script.js
@@ -1,27 +1,22 @@
 document.addEventListener("DOMContentLoaded", function(_) {
-	// When the user scrolls the page, exec sticknav()
-	window.onscroll = function() {
-		console.log("onscroll")
-		sticknav()
-	};
-
 	// Get the navbar
 	var navbar = document.getElementById("navbar");
-	console.log("foo", navbar)
 
 	// Get the offset position of the navbar
 	var sticky = navbar.offsetTop;
 
 	// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
 	function sticknav() {
-		console.log("scrollY", window.scrollY, "sticky", sticky)
 		if (window.scrollY >= sticky) {
-			console.log("adding sticky", navbar.classList)
 			navbar.classList.add("sticky")
 			return
 		}
 
-		console.log("removing sticky", navbar.classList)
 		navbar.classList.remove("sticky");
 	}
+
+	// When the user scrolls the page, exec sticknav()
+	window.onscroll = function() {
+		sticknav()
+	};
 });
