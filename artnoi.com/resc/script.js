f8e2db803b3cb21dffc842aba34fde040e338667
Prem Phansuriyanon <prem.p@lmwn.com>
2025-02-26 12:59:13 +0700

    update(artnoi.com): use const

--- artnoi.com/resc/script.js
+++ artnoi.com/resc/script.js
@@ -1,9 +1,9 @@
 document.addEventListener("DOMContentLoaded", function(_) {
 	// Get the navbar
-	var navbar = document.getElementById("navbar");
+	const navbar = document.getElementById("navbar");
 
 	// Get the offset position of the navbar
-	var sticky = navbar.offsetTop;
+	const sticky = navbar.offsetTop;
 
 	// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
 	function sticknav() {
